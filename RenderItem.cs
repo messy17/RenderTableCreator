d061b3f7ac03577a8dd1af2e95db3418893db0a3
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2021-10-06 16:52:09 +0100

    Created dataclass for render items

---
+++ RenderItem.cs
@@ -0,0 +1,16 @@
+﻿namespace RenderTableCreator
+{
+    internal class RenderItem
+    {
+        public string ImageName { get; set; }
+        public string Description { get; set; }
+        public int LineNumber { get; set; }
+
+        public RenderItem(string _imageName, string _description, int lineNumber)
+        {
+            ImageName = _imageName;
+            Description = _description;
+            LineNumber = lineNumber;
+        }
+    }
+}
