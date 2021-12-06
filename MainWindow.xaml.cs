2ee8f717c105d54f6bc4fb2a36c06d4d4a068c67
Mike Stephens <mstep592@hotmail.com>
2021-12-05 17:40:58 -0800

    - Remove depedency on Office automation using Spire.doc (free version) to create docx and pdfs witout needing Office. - Added altdocument.cs, which uses Spire and deprecates document.cs, which used Office automation. This handles ALL document creation. - Excluded document.cs from the project, but remains on the file system. - replaced the textblock and ScrollViewer in mainwindow.xaml with a read-only textbox that enables the user to copy the output warnings to the clipboard. - mainwindow.xaml.cs - Changed the type of the scenes object from Dictionary<string,RenderItem> to SortedDictionary<string,RenderItem>. - mainwindow.xaml.cs - Added logic to CreateRenderTableButton_Click to clear all the data from the previous creation to enable the user to process multiple files without closing the application. - mainwindow.xaml.cs - Added new CreateRenderItem2 and updated the logic to detect missing render descriptions and conflicting render descriptions. the method also noramlizes scene descriptions by removing # and trimming the string. Updated error string with conflicting descriptions to reference the original description and the conflict line numbers. - mainwindow.xaml.cs - Added new CreateDocument2 and update the report contents to match the output from the previous python version. This includes adding a render count for unique renders for the scene unde the Scene Notes and adding an Occurrences column to the rendertable output. - mainwindow.xaml.cs - Replaced calls to CreateRenderItem with CreateRenderItem2. - mainwindow.xaml.cs - Replaced calls to CreateDocument with CreateDocument2. - mainwindow.xaml.cs - Updated the FailedConvert method. Changed the string to include the scene name for the failure so it appears in the output window and gives the user context about the file that was processed. Also, gives more context when the user copies and pases the output window (sharing errors with transcribers). - mainwindow.xaml.cs - Added a OrderList method. This implements a traditional bubble sort to change the order of the renderItems in the list from Lexicographical to truely numeric-alphabetic ascending. Lexicographical order is not ascending order when numbers are strings. OrderList sorts all the scenes by numberic value first and then sorts the scenes with the same numeric value using alphabetic. All sorting is ascending( low to high for both numbers and letters). - mainwindow.xaml.cs - Added a SwampListEntries method as a helper method for OrderList. - mainwindow.xaml.cs - Added a GetImageNumber method as a helper method for OrderList. The method takes the input filename of an image and returns the numeric value of the image after the underscore. - mainwindow.xaml.cs - Updated the SuccessfulConvert method. The method reuses the renderList object to get a sorted list from the scenes SortedDictionary. The renderlist is then sorted using a newly added OrderList method. - renderItem.cs - Added a integer property "RefCount" to store the number of times a rendered scene is referenced. This value is used to populate the Occurrences column that was added to the render table. The default value for refcount is one (1) when a new renderItem is created. - Added a red "Oscar Six" icon and associated it with the application. - Adusted the Target Platform to be X64. - Added a feature flag to enable saving reports in PDF format (RTC_PDF_OUTPUT=1).

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.Office.Interop.Word;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using Spire.Doc.Documents; 
 
 namespace RenderTableCreator
 {
     public partial class MainWindow : System.Windows.Window
     {
-        private List<RenderItem> renderItems = new();
-        private Dictionary<string, RenderItem> scenes = new();
+        private List<RenderItem> renderList = new();
+        private SortedDictionary<string, RenderItem> scenes = new();
 
         private string selectedFile;
         private string renderTableFile;
@@ -54,6 +54,15 @@ namespace RenderTableCreator
 
         private void CreateRenderTableButton_Click(object sender, RoutedEventArgs e)
         {
+            // Reset the state each time the create render table button is clicked
+            
+            WindowOutput.Clear();
+            outputLog.Clear(); 
+            notes.Clear();
+            scenes.Clear(); 
+            errorText = "ERRORS FOUND IN TRANSCRIPT. FIX THEM AND TRY AGAIN:";
+            warnText = "WARNINGS:";
+
             List<string> speakers = new();
 
             StreamReader file = new(selectedFile);
@@ -74,98 +83,179 @@ namespace RenderTableCreator
                 }
                 else { inNotes = false; }
 
-                CreateRenderItem(line, lineNumber);
+                CreateRenderItem2(line, lineNumber); 
             }
 
             if (errorText == "ERRORS FOUND IN TRANSCRIPT. FIX THEM AND TRY AGAIN:" && warnText == "WARNINGS:")
             {
+                
                 SuccessfulConvert();
             }
             else { FailedConvert(); }
+
         }
 
-        private void CreateRenderItem(string line, int lineNumber)
+        private void CreateRenderItem2(string line, int lineNumber)
         {
-            if (line.StartsWith("scene") || line.StartsWith("show"))
+            if(line.StartsWith("scene") || line.StartsWith("show"))
             {
                 string[] lineArgs = line.Split(' ');
                 string imageName = lineArgs[1];
-                if (imageName == "black") { return; }
-                if (lineArgs.Length <= 2) { return; }
+                
+                if (0 == String.Compare(imageName, "black", true))
+                    return;
 
-                string imageDesc = string.Join(' ', lineArgs[3..]);
+                string imageDesc = String.Empty; 
+                
+                if (lineArgs.Length > 2)
+                    imageDesc = string.Join(' ',lineArgs[3..]);
 
-                if (scenes.ContainsKey(imageName) && imageDesc != scenes[imageName].Description)
+                // Normalize the description
+                imageDesc = imageDesc.Replace('#', ' ').Trim();
+
+                // current scene not in the list; New Scene
+                if(!scenes.ContainsKey(imageName))
                 {
-                    errorText += $"\n{imageName}: Conflicting description found at line {lineNumber}";
+                    // New scene without a render description; Error case
+                    if (String.IsNullOrEmpty(imageDesc))
+                    {
+                        warnText += $"\n{imageName}: Missing description at line {lineNumber}";
+                    }
+                    // New scene with a proper render description; add to list
+                    else
+                    {
+                        scenes.Add(imageName, new RenderItem(
+                            imageName,
+                            imageDesc,
+                            lineNumber));
+                    }
                 }
-                else if (imageDesc == string.Empty)
-                {
-                    warnText += $"\n{imageName}: Missing description";
-                }
+                // current scene is in the list; Potential reuse
                 else
                 {
-                    renderItems.Add(new RenderItem(imageName, imageDesc, lineNumber));
+                    // Scene Reuse; legit use case 
+                    if(String.IsNullOrEmpty(imageDesc))
+                    {
+                        scenes[imageName].RefCount++;
+                    }                   
+                    //Existing scene with a different render description than previous; Error case
+                    else if(!imageDesc.Equals(scenes[imageName].Description))
+                    {
+                        int originalLineNumber = scenes[imageName].LineNumber;
+                        
+                        errorText += $"\n{imageName}: Conflicting description found at line {lineNumber} with orignal description at line {originalLineNumber}.";
+                    }
+
                 }
             }
         }
-
-        private void CreateDocument()
+        
+        private void CreateDocument2()
         {
-            Document document = new();
-            document.AddHeading("Title", $"{sceneName} Render Table");
-
-            document.AddHeading("Heading 1", "Scene Notes:");
-            document.AddParagraph(string.Join("\n", notes));
-
-            document.AddHeading("Heading 1", "Render Table:");
-
-            // Create render table
-            Table table = document.CreateTable(2, renderItems.Count);
-
-            renderItems.Sort(delegate (RenderItem x, RenderItem y)
-            {
-                return x.ImageName.CompareTo(y.ImageName);
-            });
-
-            int index = 0;
-            foreach (Row row in table.Rows)
-            {
-                if (row.Index == 1)
-                {
-                    row.Range.Font.Bold = 1;
-                    row.Cells[1].Range.Text = "Scene";
-                    row.Cells[2].Range.Text = "Description";
-                }
-                else
-                {
-                    row.Cells[1].Range.Text = renderItems[index].ImageName;
-                    row.Cells[2].Range.Text = renderItems[index].Description;
-                }
-                index++;
-            }
+            AltDocument document = new();
+  
+            document.AddHeading(String.Format("{0} Render Table", sceneName), BuiltinStyle.Title);
+            document.AddHeading("Scene Notes:", BuiltinStyle.Heading1);
+            document.AddParagraph(String.Join("\n", notes));
+            document.AddParagraph(String.Format("Render count: {0}", renderList.Count));
+            document.AddHeading("Render Table:", BuiltinStyle.Heading1);
+            document.AddParagraph(String.Empty);
+            
+            String[] tableHeadings = { "Scene", "Description", "Occurrences" };
+            document.AddTable(tableHeadings, renderList);
 
             document.SaveDocument(renderTableFile);
-            AddLog("Render Table Created Successfully");
-        }
+            AddLog(String.Format("Render Table Created Successfully for {0}", sceneName));
+
+        }                
 
         private void SuccessfulConvert()
         {
-            CreateDocument();
+            // CreateDocument();
+            renderList = scenes.Values.ToList();
+            OrderList(ref renderList); 
+            CreateDocument2(); 
         }
 
         private void FailedConvert()
         {
-            AddLog("Failed to create render table");
+            AddLog(String.Format("Failed to create render table for {0}.", sceneName));
             if (errorText != "ERRORS FOUND IN TRANSCRIPT. FIX THEM AND TRY AGAIN:")
             {
                 AddLog(errorText);
             }
-
             if (warnText != "WARNINGS:")
             {
                 AddLog(warnText);
             }
+        }
+
+        private void OrderList(ref List<RenderItem> list)
+        {
+            bool change = false; 
+            do
+            {                
+
+                for (int previous = 0; previous < list.Count - 1; previous++)
+                {
+                    int current = previous + 1;
+
+                    int cInt = GetImageNumber(list[current].ImageName);
+                    int pInt = GetImageNumber(list[previous].ImageName);
+
+                    if (cInt < pInt)
+                    {
+                        SwampListEntries(previous, current, ref list);
+                        change = true; 
+
+                    }
+                    else if (cInt == pInt)
+                    {
+                        char cLetter = list[current].ImageName[list[current].ImageName.Length - 1];
+                        char pLetter = list[previous].ImageName[list[previous].ImageName.Length - 1];
+
+                        if (cLetter < pLetter)
+                        {
+                            SwampListEntries(previous, current, ref list);
+                            change = true; 
+                        }
+                    }
+
+                }
+            } while (change);
+            
+        }
+
+        private static void SwampListEntries(int posA, int posB,  ref List<RenderItem> list)
+        {
+            RenderItem placholder = list[posB];
+            list[posB] = list[posA];
+            list[posA] = placholder;
+        }
+
+        private static int GetImageNumber(string imageName)
+        {            
+            int retval = -1;
+
+            if (String.IsNullOrEmpty(imageName))
+                return retval; 
+
+            int underScorePos = imageName.IndexOf("_");
+
+            if (underScorePos == -1)
+                return retval; 
+            
+            String intStr = imageName.Substring(underScorePos + 1, imageName.Length - (underScorePos + 1));
+            if (!Int32.TryParse(intStr, out retval))
+            {
+                intStr = intStr.Remove(intStr.Length - 1);
+
+                if (!Int32.TryParse(intStr, out retval))
+                    retval = -1;     
+            }
+
+            return retval; 
+
         }
     }
 }
